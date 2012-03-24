e9a414d20cbf3e6cbcf8e8e0573811590e720be3
Dương "Yang" ヤン Nguyễn <cmpitg@gmail.com>
2012-03-24 15:07:31 +0700

    chin

--- number_to_speech.cpp
+++ number_to_speech.cpp
@@ -8,11 +8,13 @@ const char *number_to_speech (int number) {
     else if (number == 1)
         return "one base-5";
     else if (number == 2)
+
         return "two base-5";
     else if (number == 3)
         return "three base-5";
     else if (number == 4)
         return "four base-5";
+    
 }
 
 TEST (DummyTest, DummyCase) {
@@ -21,6 +23,7 @@ TEST (DummyTest, DummyCase) {
     EXPECT_STREQ ("two base-5",  number_to_speech (2));
     EXPECT_STREQ ("three base-5",  number_to_speech (3));
     EXPECT_STREQ ("four base-5",  number_to_speech (4));
+    EXPECT_STREQ ("one zero base-5", number_to_speech(10) ); 
 }
 
 int main (int argc, char *argv[])
