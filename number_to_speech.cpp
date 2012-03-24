e864d464af13e279e6f0289a84852ad9a523d2c3
Dương "Yang" ヤン Nguyễn <cmpitg@gmail.com>
2012-03-24 14:54:54 +0700

    cmpitg

--- number_to_speech.cpp
+++ number_to_speech.cpp
@@ -1,5 +1,10 @@
 #include <limits.h>
 #include "gtest/gtest.h"
+#include <string>
+
+const char *number_to_speech (int number) {
+    return "zero base-5";
+}
 
 TEST (DummyTest, DummyCase) {
     EXPECT_STREQ ("zero base-5", number_to_speech (0));
